ea7d189836253413aba80a76071c5d232506e72b
Taylor Cox <taylor@posidev.com>
2025-03-29 20:40:46 -0400

    3 column scoreboard

--- resources/views/livewire/pages/games/view-games.blade.php
+++ resources/views/livewire/pages/games/view-games.blade.php
@@ -25,7 +25,7 @@
 
     </div>
 
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
 
         @foreach ($this->games as $game)
             <x-games.card :game="$game" />
