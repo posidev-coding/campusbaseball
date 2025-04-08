977ce4ee7ca26f2e2f641a22e8e0e2b130703df3
Taylor Cox <taylor@posidev.com>
2025-04-07 20:03:00 -0400

    Debugging scheduler

--- routes/console.php
+++ routes/console.php
@@ -8,14 +8,14 @@ use App\Jobs\Feeds\SyncRankings;
 use Illuminate\Support\Facades\Schedule;
 
 // Game Syncing
-Schedule::job(new SyncGames('live'))->everyFiveMinutes()->unlessBetween('1:00', '11:00');
-Schedule::job(new SyncGames('today'))->hourly()->between('6:00', '23:00');
-Schedule::job(new SyncGames('tomorrow'))->everyOddHour()->between('10:00', '23:00');
-Schedule::job(new SyncGames('yesterday'))->twiceDaily(2, 8);
+Schedule::job(new SyncGames('live'))->everyFiveMinutes();
+Schedule::job(new SyncGames('today'))->hourly();
+Schedule::job(new SyncGames('tomorrow'))->everyOddHour();
+Schedule::job(new SyncGames('yesterday'))->twiceDaily();
 Schedule::job(new SyncGames('future'))->dailyAt(6);
 
 // Conferences, Teams, Articles & Rankings
 Schedule::job(new SyncGroups())->everyTwoHours();
 Schedule::job(new SyncTeams())->dailyAt(5);
 Schedule::job(new SyncArticles())->everyTwoHours(15);
-Schedule::job(new SyncRankings())->everyOddHour()->mondays()->between('4:00', '4:00');
+Schedule::job(new SyncRankings())->everyOddHour()->mondays();
