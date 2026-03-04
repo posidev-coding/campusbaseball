f7df62b107d8eb80320addf1898b7c4ba1a6a5ac
Posidev <153259770+posidev-coding@users.noreply.github.com>
2026-03-04 15:49:34 -0500

    Refactor rankings query to include season_id filter

--- app/Livewire/Rankings/ViewRankings.php
+++ app/Livewire/Rankings/ViewRankings.php
@@ -14,12 +14,11 @@ class ViewRankings extends Component
     public function mount()
     {
         $this->week = Ranking::where('season_id', config('espn.year'))->max('week_nbr');
-        // dd($this->week);2
     }
 
     #[\Livewire\Attributes\Computed]
     public function rankings()
     {
-        return Ranking::where('week_nbr', $this->week)->orderBy('current')->get();
+        return Ranking::where('season_id', config('espn.year'))->where('week_nbr', $this->week)->orderBy('current')->get();
     }
 }
